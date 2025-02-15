c3a43234b65e12342fbf654f806d7cf9217095c3
usernames122 <sigge.arkestal@gmail.com>
2025-02-15 18:55:01 +0100

    Remove rocket emoji cuz it was interfering with the windows build for some reason

--- hooks/hook-gpt4all.py
+++ hooks/hook-gpt4all.py
@@ -2,4 +2,4 @@ from PyInstaller.utils.hooks import collect_dynamic_libs
 
 # Automatically collect all shared libraries
 binaries = collect_dynamic_libs('gpt4all')
-print(f"🚀 Hook executed at compile time: {binaries}")
+print(f"Hook executed at compile time: {binaries}")
